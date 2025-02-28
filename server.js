f7fbbdbabed58090bdbedf1730af211b49e102ca
sheehzz <2375576@brunel.ac.uk>
2025-02-28 12:26:37 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log(' MongoDB connected'))
+.catch(err => console.error(' MongoDB connection error:', err));
+
+// Simple API Route
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
